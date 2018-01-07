1297306745d8188d33c7d8606af39b0c931b2d90
mgyongyosi <mgyongyosi@users.noreply.github.com>
2018-01-07 16:25:46 +0100

    Added test for AttributeArgumentSyntaxExtensions.CanAssignTo

---
+++ src/nunit.analyzers.tests/Targets/Extensions/AttributeArgumentSyntaxExtensionsTestsCanAssignToWhenArgumentIsImplicitlyTypedArrayAndAssignable.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnit.Analyzers.Tests.Targets.Extensions
+{
+	public sealed class AttributeArgumentSyntaxExtensionsTestsCanAssignToWhenArgumentIsImplicitlyTypedArrayAndAssignable
+    {
+		[Arguments(new[] { "a", "b", "c" })]
+		public void Foo(string[] inputs) { }
+
+		[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+		public sealed class ArgumentsAttribute
+			: Attribute
+		{
+			public ArgumentsAttribute(int? x) { }
+		}
+	}
+}
---
+++ src/nunit.analyzers.tests/Targets/Extensions/AttributeArgumentSyntaxExtensionsTestsCanAssignToWhenArgumentIsImplicitlyTypedArrayAndNotAssignable.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnit.Analyzers.Tests.Targets.Extensions
+{
+	public sealed class AttributeArgumentSyntaxExtensionsTestsCanAssignToWhenArgumentIsImplicitlyTypedArrayAndNotAssignable
+    {
+		[Arguments(new[] { "a", "b", "c" })]
+		public void Foo(int[] inputs) { }
+
+		[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+		public sealed class ArgumentsAttribute
+			: Attribute
+		{
+			public ArgumentsAttribute(int? x) { }
+		}
+	}
+}
