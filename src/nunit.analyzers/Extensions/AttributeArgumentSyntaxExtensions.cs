b4091ceaebb9a090791a9d2101abca3328ae3fdf
mgyongyosi <mgyongyosi@users.noreply.github.com>
2018-03-04 16:12:30 +0100

    AttributeArgumentSyntaxExtensions.CanAssignTo refactored

--- src/nunit.analyzers/Extensions/AttributeArgumentSyntaxExtensions.cs
+++ src/nunit.analyzers/Extensions/AttributeArgumentSyntaxExtensions.cs
@@ -7,81 +7,78 @@ using System.Linq;
 
 namespace NUnit.Analyzers.Extensions
 {
-	internal static class AttributeArgumentSyntaxExtensions
-	{
-		internal static bool CanAssignTo(this AttributeArgumentSyntax @this, ITypeSymbol target, SemanticModel model)
-		{
-			// See https://github.com/nunit/nunit/blob/master/src/NUnitFramework/framework/Attributes/TestCaseAttribute.cs#L363
-			// for the reasoning behind this implementation.
-			object argumentValue = null;
-			if(@this.Expression is LiteralExpressionSyntax)
-				argumentValue = (@this.Expression as LiteralExpressionSyntax).Token.Value;
+    internal static class AttributeArgumentSyntaxExtensions
+    {
+        internal static bool CanAssignTo(this AttributeArgumentSyntax @this, ITypeSymbol target, SemanticModel model)
+        {
+            //See https://github.com/nunit/nunit/blob/master/src/NUnitFramework/framework/Attributes/TestCaseAttribute.cs#L363
+            //for the reasoning behind this implementation.
+            object argumentValue = null;
+            if(@this.Expression is LiteralExpressionSyntax)
+            {
+                argumentValue = (@this.Expression as LiteralExpressionSyntax).Token.Value;
+            }
 
-            		if(@this.Expression is ImplicitArrayCreationExpressionSyntax)
-                		argumentValue = ((@this.Expression as ImplicitArrayCreationExpressionSyntax).Initializer.Expressions[0] as LiteralExpressionSyntax).Token.Value;
+            TypeInfo sourceTypeInfo = model.GetTypeInfo(@this.Expression);
+            ITypeSymbol argumentType = sourceTypeInfo.Type;
 
-			if (argumentValue == null)
-			{
-				return target.IsReferenceType ||
-					target.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T;
-			}
-			else
-			{
-				var argumentType = model.Compilation.GetTypeByMetadataName(argumentValue.GetType().FullName);
-				var targetType = GetTargetType(target);
+            if(sourceTypeInfo.Type == null)
+            {
+                return target.IsReferenceType ||
+                    target.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T;
+            }
+            else
+            {
+                var targetType = GetTargetType(target);
+                if(targetType.IsAssignableFrom(argumentType))
+                {
+                    return true;
+                }
+                else
+                {
+                    var canConvert = false;
 
-				if (targetType.IsAssignableFrom(argumentType))
-				{
-					return true;
-				}
-				else
-				{
-					var canConvert = false;
+                    if(targetType.SpecialType == SpecialType.System_Int16 || targetType.SpecialType == SpecialType.System_Byte ||
+                        targetType.SpecialType == SpecialType.System_SByte || targetType.SpecialType == SpecialType.System_Double)
+                    {
+                        canConvert = argumentType.SpecialType == SpecialType.System_Int32;
+                    }
+                    else if(targetType.SpecialType == SpecialType.System_Decimal)
+                    {
+                        canConvert = argumentType.SpecialType == SpecialType.System_Double ||
+                            argumentType.SpecialType == SpecialType.System_String ||
+                            argumentType.SpecialType == SpecialType.System_Int32;
+                    }
+                    else if(targetType.SpecialType == SpecialType.System_DateTime)
+                    {
+                        canConvert = argumentType.SpecialType == SpecialType.System_String;
+                    }
 
-					if (targetType.SpecialType == SpecialType.System_Int16 || targetType.SpecialType == SpecialType.System_Byte ||
-						targetType.SpecialType == SpecialType.System_SByte || targetType.SpecialType == SpecialType.System_Double)
-					{
-						canConvert = argumentType.SpecialType == SpecialType.System_Int32;
-					}
-					else if (targetType.SpecialType == SpecialType.System_Decimal)
-					{
-						canConvert = argumentType.SpecialType == SpecialType.System_Double ||
-							argumentType.SpecialType == SpecialType.System_String ||
-							argumentType.SpecialType == SpecialType.System_Int32;
-					}
-					else if (targetType.SpecialType == SpecialType.System_DateTime)
-					{
-						canConvert = argumentType.SpecialType == SpecialType.System_String;
-					}
+                    if(canConvert)
+                    {
+                        return AttributeArgumentSyntaxExtensions.TryChangeType(targetType, argumentValue);
+                    }
+                    else if(argumentType.SpecialType == SpecialType.System_String &&
+                        model.Compilation.GetTypeByMetadataName(typeof(TimeSpan).FullName).IsAssignableFrom(targetType))
+                    {
+                        var outValue = default(TimeSpan);
+                        canConvert = TimeSpan.TryParse(argumentValue as string, out outValue);
+                    }
 
-					if (canConvert)
-					{
-						return AttributeArgumentSyntaxExtensions.TryChangeType(targetType, argumentValue);
-					}
-					else if(argumentType.SpecialType == SpecialType.System_String &&
-						model.Compilation.GetTypeByMetadataName(typeof(TimeSpan).FullName).IsAssignableFrom(targetType))
-					{
-						var outValue = default(TimeSpan);
-						canConvert = TimeSpan.TryParse(argumentValue as string, out outValue);
-					}
-
-					return canConvert;
-				}
-			}
-		}
-
-		private static ITypeSymbol GetTargetType(ITypeSymbol target)
+                    return canConvert;
+                }
+            }
+        }
+        
+        private static ITypeSymbol GetTargetType(ITypeSymbol target)
 		{
 			if(target.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T)
 				return (target as INamedTypeSymbol).TypeArguments.ToArray()[0];
 
-			if(target is IArrayTypeSymbol)
-				return (target as IArrayTypeSymbol).ElementType;
-
-			return target;
+            return target;
 		}
 
-        	private static bool TryChangeType(ITypeSymbol targetType, object argumentValue)
+        private static bool TryChangeType(ITypeSymbol targetType, object argumentValue)
 		{
 			var targetReflectionType = Type.GetType(
 				AttributeArgumentSyntaxExtensions.GetFullName(targetType), false);
--- src/nunit.analyzers/Extensions/ITypeSymbolExtensions.cs
+++ src/nunit.analyzers/Extensions/ITypeSymbolExtensions.cs
@@ -6,17 +6,16 @@ namespace NUnit.Analyzers.Extensions
 {
 	internal static class ITypeSymbolExtensions
 	{
-		internal static bool IsAssignableFrom(this ITypeSymbol @this, ITypeSymbol other)
-		{
-			return @this != null &&
-				other != null &&
-				((@this.MetadataName == other.MetadataName && 
-					@this.ContainingAssembly.MetadataName == other.ContainingAssembly.MetadataName) ||
-				@this.IsAssignableFrom(other.BaseType) ||
-				other.Interfaces.Any(_ => @this.IsAssignableFrom(_)));
-		}
+        internal static bool IsAssignableFrom(this ITypeSymbol @this, ITypeSymbol other)
+        {
+            return @this != null &&
+                other != null &&
+                (@this.Equals(other) ||
+                @this.IsAssignableFrom(other.BaseType) ||
+                other.Interfaces.Any(_ => @this.IsAssignableFrom(_)));
+        }
 
-		internal static bool IsAssert(this ITypeSymbol @this)
+        internal static bool IsAssert(this ITypeSymbol @this)
 		{
 			return @this != null &&
 				typeof(Assert).AssemblyQualifiedName.Contains(@this.ContainingAssembly.Name) &&
