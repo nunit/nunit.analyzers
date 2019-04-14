276bdc42edf4df1e512d5a20f4bde426910903b3
Mikkel Nylander Bundgaard <mikkelbu@users.noreply.github.com>
2019-04-14 23:27:42 +0400

    Apply suggestions from code review
    Co-Authored-By: Dreamescaper <liakh.oleksandr32@gmail.com>

--- src/nunit.analyzers/Constants/IgnoreUsageAnalyzerConstants.cs
+++ src/nunit.analyzers/Constants/IgnoreUsageAnalyzerConstants.cs
@@ -2,7 +2,7 @@ namespace NUnit.Analyzers.Constants
 {
     internal static class IgnoreCaseUsageAnalyzerConstants
     {
-        internal const string Title = "Find Incorrect IgnoreCase usage";
-        internal const string Message = "IgnoreCase modifier can only be used for string or char argument!";
+        internal const string Title = "Find incorrect IgnoreCase usage";
+        internal const string Message = "IgnoreCase modifier should only be used for string or char arguments.";
     }
 }
