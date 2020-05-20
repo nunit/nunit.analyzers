ba53fd30bb9cf9c2bce7462dcddc9af825896d36
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2020-05-20 17:08:05 +0200

    fix: Correct message

--- src/nunit.analyzers/Constants/TestMethodUsageAnalyzerConstants.cs
+++ src/nunit.analyzers/Constants/TestMethodUsageAnalyzerConstants.cs
@@ -3,7 +3,7 @@ namespace NUnit.Analyzers.Constants
     internal static class TestMethodUsageAnalyzerConstants
     {
         internal const string ExpectedResultTypeMismatchTitle = "The type of the value specified via ExpectedResult must match the return type of the method.";
-        internal const string ExpectedResultTypeMismatchMessage = "The value specified via ExpectedResult cannot match the method's return type '{0}'.";
+        internal const string ExpectedResultTypeMismatchMessage = "The type of the value specified via ExpectedResult cannot match the method's return type '{0}'.";
         internal const string ExpectedResultTypeMismatchDescription = "The type of the value specified via ExpectedResult must match the return type of the method. " +
             "Otherwise, this will lead to an error at run-time.";
 
