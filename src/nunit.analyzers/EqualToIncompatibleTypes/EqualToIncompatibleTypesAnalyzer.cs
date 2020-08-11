229566de0cb839282bc88fff8a5cc033ef3373ec
Manfred Brands <m.brands@fugro.com>
2020-08-11 12:31:36 +0800

    EqualToIncompatibleTypesAnalyzer: Added support for classic Assert

--- src/nunit.analyzers/EqualToIncompatibleTypes/EqualToIncompatibleTypesAnalyzer.cs
+++ src/nunit.analyzers/EqualToIncompatibleTypes/EqualToIncompatibleTypesAnalyzer.cs
@@ -25,43 +25,65 @@ namespace NUnit.Analyzers.EqualToIncompatibleTypes
 
         protected override void AnalyzeAssertInvocation(OperationAnalysisContext context, IInvocationOperation assertOperation)
         {
-            if (!AssertHelper.TryGetActualAndConstraintOperations(assertOperation,
-                out var actualOperation, out var constraintExpression))
-            {
-                return;
-            }
+            IOperation? actualOperation;
+            IOperation? expectedOperation;
 
-            foreach (var constraintPartExpression in constraintExpression.ConstraintParts)
+            if (assertOperation.TargetMethod.Name.Equals(NunitFrameworkConstants.NameOfAssertAreEqual) ||
+                assertOperation.TargetMethod.Name.Equals(NunitFrameworkConstants.NameOfAssertAreNotEqual))
             {
-                if (HasIncompatiblePrefixes(constraintPartExpression)
-                    || HasCustomEqualityComparer(constraintPartExpression)
-                    || constraintPartExpression.HasUnknownExpressions())
+                actualOperation = assertOperation.GetArgumentOperation(NunitFrameworkConstants.NameOfActualParameter);
+                expectedOperation = assertOperation.GetArgumentOperation(NunitFrameworkConstants.NameOfExpectedParameter);
+
+                CheckActualVsExpectedOperation(context, actualOperation, expectedOperation);
+            }
+            else
+            {
+                if (!AssertHelper.TryGetActualAndConstraintOperations(assertOperation,
+                    out actualOperation, out var constraintExpression))
                 {
                     return;
                 }
 
-                var constraintMethod = constraintPartExpression.GetConstraintMethod();
-
-                if (constraintMethod?.Name != NunitFrameworkConstants.NameOfIsEqualTo
-                    || constraintMethod.ReturnType?.GetFullMetadataName() != NunitFrameworkConstants.FullNameOfEqualToConstraint)
+                foreach (var constraintPartExpression in constraintExpression.ConstraintParts)
                 {
-                    continue;
+                    if (HasIncompatiblePrefixes(constraintPartExpression)
+                        || HasCustomEqualityComparer(constraintPartExpression)
+                        || constraintPartExpression.HasUnknownExpressions())
+                    {
+                        return;
+                    }
+
+                    var constraintMethod = constraintPartExpression.GetConstraintMethod();
+
+                    if (constraintMethod?.Name != NunitFrameworkConstants.NameOfIsEqualTo
+                        || constraintMethod.ReturnType?.GetFullMetadataName() != NunitFrameworkConstants.FullNameOfEqualToConstraint)
+                    {
+                        continue;
+                    }
+
+                    expectedOperation = constraintPartExpression.GetExpectedArgument();
+
+                    CheckActualVsExpectedOperation(context, actualOperation, expectedOperation);
                 }
+            }
+        }
 
-                var actualType = AssertHelper.GetUnwrappedActualType(actualOperation);
+        private static void CheckActualVsExpectedOperation(OperationAnalysisContext context, IOperation? actualOperation, IOperation? expectedOperation)
+        {
+            if (actualOperation == null || expectedOperation == null)
+                return;
 
-                var expectedArgumentExpression = constraintPartExpression.GetExpectedArgument();
-                var expectedType = expectedArgumentExpression?.Type;
+            var actualType = AssertHelper.GetUnwrappedActualType(actualOperation);
+            var expectedType = expectedOperation.Type;
 
-                if (actualType == null || expectedArgumentExpression == null || expectedType == null)
-                    continue;
+            if (actualType == null || expectedType == null)
+                return;
 
-                if (!NUnitEqualityComparerHelper.CanBeEqual(actualType, expectedType, context.Compilation))
-                {
-                    context.ReportDiagnostic(Diagnostic.Create(
-                        descriptor,
-                        expectedArgumentExpression.Syntax.GetLocation()));
-                }
+            if (!NUnitEqualityComparerHelper.CanBeEqual(actualType, expectedType, context.Compilation))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(
+                    descriptor,
+                    expectedOperation.Syntax.GetLocation()));
             }
         }
 
