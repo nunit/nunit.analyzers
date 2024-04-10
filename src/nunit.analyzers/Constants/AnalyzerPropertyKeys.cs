7700c2069781879efbd1eba7dd4f5bf16a32e721
Manfred Brands <manfred-brands@users.noreply.github.com>
2024-04-10 12:15:24 +0800

    We no longer need HasToleranceValue

--- src/nunit.analyzers/Constants/AnalyzerPropertyKeys.cs
+++ src/nunit.analyzers/Constants/AnalyzerPropertyKeys.cs
@@ -2,7 +2,6 @@ namespace NUnit.Analyzers.Constants
 {
     internal static class AnalyzerPropertyKeys
     {
-        internal const string HasToleranceValue = nameof(AnalyzerPropertyKeys.HasToleranceValue);
         internal const string ModelName = nameof(AnalyzerPropertyKeys.ModelName);
         internal const string MinimumNumberOfArguments = nameof(AnalyzerPropertyKeys.MinimumNumberOfArguments);
         internal const string ComparerParameterIndex = nameof(AnalyzerPropertyKeys.ComparerParameterIndex);
