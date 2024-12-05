d4dbd1438ee2b8fe4d067d55d6c0ed4008995826
Jihoon Park <bartleby2718@gmail.com>
2024-12-04 20:15:26 -0500

    Fix the rest

--- src/nunit.analyzers/UseAssertThatAsync/UseAssertThatAsyncConstants.cs
+++ src/nunit.analyzers/UseAssertThatAsync/UseAssertThatAsyncConstants.cs
@@ -3,6 +3,6 @@ namespace NUnit.Analyzers.UseAssertThatAsync;
 internal static class UseAssertThatAsyncConstants
 {
     internal const string Title = "Use Assert.ThatAsync";
-    internal const string Message = "Replace Assert.That with Assert.ThatAsync"; // TODO:
-    internal const string Description = "Use Assert.ThatAsync to modernize and simplify testing asynchronous code."; // TODO:
+    internal const string Message = "Replace Assert.That with Assert.ThatAsync";
+    internal const string Description = "You can use Assert.ThatAsync to assert asynchronously.";
 }
