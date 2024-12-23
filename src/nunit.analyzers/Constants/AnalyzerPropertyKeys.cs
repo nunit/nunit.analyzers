9fef008179e34c05ffa355a656937325abe979eb
Manfred Brands <manfred-brands@users.noreply.github.com>
2024-12-23 12:09:16 +0800

    Add CodeFix to add `using (Assert.EnterMultipleScope())`
    Allow the user to select the Assert.Multiple codefix

--- src/nunit.analyzers/Constants/AnalyzerPropertyKeys.cs
+++ src/nunit.analyzers/Constants/AnalyzerPropertyKeys.cs
@@ -5,5 +5,7 @@ namespace NUnit.Analyzers.Constants
         internal const string ModelName = nameof(AnalyzerPropertyKeys.ModelName);
         internal const string ArgsIsArray = nameof(AnalyzerPropertyKeys.ArgsIsArray);
         internal const string MinimumNumberOfArguments = nameof(AnalyzerPropertyKeys.MinimumNumberOfArguments);
+
+        internal const string SupportsEnterMultipleScope = nameof(AnalyzerPropertyKeys.SupportsEnterMultipleScope);
     }
 }
