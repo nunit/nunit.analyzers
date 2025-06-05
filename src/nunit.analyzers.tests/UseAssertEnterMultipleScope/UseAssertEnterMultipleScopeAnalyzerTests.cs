32487acee82788834a7a956676a8cf7cb7066db6
Mikkel Bundgaard <Bundgaard@copopt.com>
2025-06-05 19:02:56 +0100

    feat: Add analyzer and codefix for `Assert.Multiple` => `Assert.EnterMultipleScope`
    And similarly for `Assert.MultipleAsync`

---
+++ src/nunit.analyzers.tests/UseAssertEnterMultipleScope/UseAssertEnterMultipleScopeAnalyzerTests.cs
@@ -0,0 +1,63 @@
+#if NUNIT4
+using Gu.Roslyn.Asserts;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Analyzers.Constants;
+using NUnit.Analyzers.UseAssertEnterMultipleScope;
+
+using NUnit.Framework;
+
+namespace NUnit.Analyzers.Tests.UseAssertEnterMultipleScope
+{
+    [TestFixture]
+    public sealed class UseAssertEnterMultipleScopeAnalyzerTests
+    {
+        private readonly DiagnosticAnalyzer analyzer = new UseAssertEnterMultipleScopeAnalyzer();
+        private readonly ExpectedDiagnostic diagnostic = ExpectedDiagnostic.Create(AnalyzerIdentifiers.UseAssertEnterMultipleScope);
+
+        [Test]
+        public void AnalyzeWhenMultipleIsUsed()
+        {
+            var testCode = TestUtility.WrapMethodInClassNamespaceAndAddUsings(@"
+        public void Test()
+        {
+            ↓Assert.Multiple(() =>
+            {
+                Assert.That(true, Is.True);
+                Assert.That(false, Is.False);
+            });
+        }");
+            RoslynAssert.Diagnostics(this.analyzer, this.diagnostic, testCode);
+        }
+
+        [Test]
+        public void AnalyzeWhenMultipleAsyncIsUsed()
+        {
+            var testCode = TestUtility.WrapMethodInClassNamespaceAndAddUsings(@"
+        public async Task Test()
+        {
+            await ↓Assert.MultipleAsync(async () =>
+            {
+                Assert.That(await Get1(), Is.Not.Null);
+                Assert.That(await Get2(), Is.Not.Null);
+            });
+
+            static Task<string?> Get1() => Task.FromResult(default(string));
+            static Task<string?> Get2() => Task.FromResult(default(string));
+        }");
+            RoslynAssert.Diagnostics(this.analyzer, this.diagnostic, testCode);
+        }
+
+        [Test]
+        public void AnalyzeWhenMultipleIsNot()
+        {
+            var testCode = TestUtility.WrapMethodInClassNamespaceAndAddUsings(@"
+        public void Test()
+        {
+            Assert.That(true, Is.True);
+            Assert.That(false, Is.False);
+        }");
+            RoslynAssert.Valid(this.analyzer, testCode);
+        }
+    }
+}
+#endif
